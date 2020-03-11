d0846149da6ea0bddc54a30ae8d28d14f824f22c
Oleksandr Vorona <oleksandr_vorona@epam.com>
2020-03-11 15:05:34 +0200

    cart-item

--- src/app/cart/components/cart-item/cart-item.component.ts
+++ src/app/cart/components/cart-item/cart-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ProductModel } from './../../../products/models/product.model';
 
 @Component({
   selector: 'app-cart-item',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart-item.component.css']
 })
 export class CartItemComponent implements OnInit {
+  @Input() model: ProductModel;
 
   constructor() { }
 
